58c729af80085854514b76016bf5b0d87196253a
Your Name <your.email@example.com>
2024-06-25 08:15:07 +0530

    hari

---
+++ script.js
@@ -0,0 +1,5 @@
+// Set the timeout to 5 seconds (5000 milliseconds)
+setTimeout(function() {
+    // Redirect to the main website
+    window.location.href = 'https://portfolio-hari-two.vercel.app/'; // Change 'main.html' to your main website URL
+}, 5000);
